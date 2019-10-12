b1c1eda12d36ebe7719c7afa0d6dcf4d1e0ab483
riv1228 <rudqhr1996@naver.com>
2019-10-13 03:25:11 +0900

    나승원 PSP 수정

--- app/src/main/java/com/example/activity1/AddActivity.java
+++ app/src/main/java/com/example/activity1/AddActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.ImageButton;
 import android.widget.Toast;
 import android.app.Activity;
@@ -39,6 +40,7 @@ public class AddActivity extends AppCompatActivity {
         mDbOpenHelper.open();
         mDbOpenHelper.create();
 
+        // foodcontroller 선언 후 해당 컨트롤러로 처리
         ib1_1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -71,7 +73,6 @@ public class AddActivity extends AppCompatActivity {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         LayoutInflater inflater_dg = getLayoutInflater();
         final View view = inflater_dg.inflate(R.layout.activity_add_popup, null);
-        builder.setTitle("식재료 추가");
         builder.setView(view);
         builder.setPositiveButton("추가", new DialogInterface.OnClickListener() {
             @Override
@@ -84,34 +85,14 @@ public class AddActivity extends AppCompatActivity {
                 Integer quantity = Integer.parseInt(qText.getText().toString());
                 String dueDate = dText.getText().toString();
 
+                // 데이터베이스 call 및 저장
                 DbOpenHelper.insertColumn(ingredient, quantity, dueDate);
 
-/*
-                adapter.addnewItem(DetectiveNote.this, category, title,main, time, suspect);
-                adapter.notifyDataSetChanged();
-*/
             }
         });
- /*       builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-
-            }
-        });
-*.
-
-  */
-
         AlertDialog dialog = builder.create();
         dialog.setCanceledOnTouchOutside(false);
         dialog.show();
 
     }
- /*   public void mOnClose(View v){
-        Intent intent = new Intent();
-        intent.putExtra("result", "Close Popup");
-        setResult(RESULT_OK, intent);
-        finish();
-  }*/
 }
